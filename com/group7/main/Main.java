f9f8cf2805cc85b9514b0848227b0b2e9011c1c7
foober123 <fabrigar.charls@gmail.com>
2025-05-09 21:38:56 +0800

    Update Main.java
    Main should be setup by now. imported packages subject to change bc idk how package paths work

--- com/group7/main/Main.java
+++ com/group7/main/Main.java
@@ -1,5 +1,19 @@
+package com.group7.main.Main;
+import com.group7.controller.SalaryController;
+import com.group7.view.SalaryGUI;
+
+
 class Main{
-  public static void main(String[] args){
-    //code here
-  }
+public static void main(String[] args) {
+   javax.swing.SwingUtilities.invokeLater(() -> {
+    SalaryModel model = new SalaryModel();
+    SalaryController controller = new SalaryController(model);
+    SalaryView view = new SalaryView(controller);
+    controller.setView(view);
+    view.createAndShowGUI();
+
+
+
+     }); 
+    }
 }
