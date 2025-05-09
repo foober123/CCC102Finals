0fd980edab6463a971054a826ae0d09bf0b36290
adnut143latik <andreplayz80@gmail.com>
2025-05-09 15:26:16 +0800

    Update SalaryController.java

---
+++ com/group7/controller/SalaryController.java
@@ -0,0 +1,10 @@
+class HourlyToSalary extends SalaryCalculator {
+    @Override
+    public double calculateNetPay(double hourlyWage) {
+        return (hourlyWage * hoursperweek*weeks) * (1 - taxRate/100.0); // Example: 40h/week, 52 weeks
+    }
+}
+class WeeklyToSalary extends SalaryCalculator{
+    @Override
+   // public double calculateNetPay(double 
+                                    }
