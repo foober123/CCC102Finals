5d10f3728fd4b7bef58228798c10b62485d31417
adnut143latik <andreplayz80@gmail.com>
2025-05-09 15:08:56 +0800

    Update SalaryModel.java

---
+++ com/group7/model/SalaryModel.java
@@ -0,0 +1,11 @@
+abstract class SalaryCalculator {
+    protected double taxRate;
+    public abstract double calculateNetPay(double gross);
+}
+
+class HourlyToSalary extends SalaryCalculator {
+    @Override
+    public double calculateNetPay(double hourlyWage) {
+        return (hourlyWage * 40 * 52) * (1 - taxRate/100); // Example: 40h/week, 52 weeks
+    }
+}
