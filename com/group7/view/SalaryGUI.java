a712cfccf75befd4fbb98b720f2ab76d955da6e8
Charlson <fabrigar.charls@gmail.com>
2025-05-10 18:45:08 +0800

    input fields to view

--- com/group7/view/SalaryGUI.java
+++ com/group7/view/SalaryGUI.java
@@ -10,6 +10,7 @@ private JFrame frame;
 private JTextField hourlyWagefield;
 private JTextField hoursPerWeekfield;
 private JTextField taxRateField;
+private JTextField resultArea;
 
 public SalaryView(SalaryController controller) {
         this.controller = controller;
@@ -21,6 +22,21 @@ frame = new JFrame("Salary Calculator");
 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 frame.setSize(350, 200);
 frame.setLocationRelativeTo(null);
+
+frame.setLayout(new FlowLayout());
+
+// Input boxes
+    frame.add(new Jlabel("Hourly Wage: "));
+    hourlyWagefield = new JTextField(10);
+    frame.add(hourlyWagefield);
+
+    frame.add(new Jlabel("Hours Per Week:"));
+    hoursPerWeekfield = new JTextField(10);
+    frame.add(hourlyWagefield);
+
+    frame.add(new Jlabel("Tax Rate: "));
+    taxRateField = new JTextField(10);
+    frame.add(taxRateField);
 
 frame.setVisible(true);
 }
