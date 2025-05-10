b4e0dca02ec5c9cbf409a72112cc16d90b89d708
sly0h <145259192+sly0h@users.noreply.github.com>
2025-05-11 01:40:55 +0800

    Update Main.java
    added code for dark mode sa calculator. Will finish when im free

--- src/com/group7/main/Main.java
+++ src/com/group7/main/Main.java
@@ -1,5 +1,6 @@
 package com.group7.main;
 
+//import com.formdev.flatlaf.FlatDarkLaf;
 import com.group7.controller.SalaryController;
 import com.group7.view.SalaryGUI;
 import com.group7.model.SalaryModel;
@@ -7,6 +8,15 @@ import java.util.Scanner;
 
 class Main{
 public static void main(String[] args) {
+
+ /*try {
+  UIManager.setLookAndFeel(new FlatDarkLaf());
+ }
+ catch (Exception e) {
+  System.err.println("Failed to initialize LaF");
+ }
+*/
+ }
    javax.swing.SwingUtilities.invokeLater(() -> {
     SalaryModel model = new SalaryModel();
     SalaryController controller = new SalaryController(model);
