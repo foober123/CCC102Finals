1dc9a0bb203c0129c033fef290e078ae90893d92
adnut143latik <andreplayz80@gmail.com>
2025-05-10 17:52:37 +0800

    Update Main.java

--- com/group7/main/Main.java
+++ com/group7/main/Main.java
@@ -1,10 +1,11 @@
 package com.group7.main.Main;
 import com.group7.controller.SalaryController;
 import com.group7.view.SalaryGUI;
-
+import java.util.Scanner;
 
 class Main{
 public static void main(String[] args) {
+   Scanner input = new Scanner(Systen.in);
    javax.swing.SwingUtilities.invokeLater(() -> {
     SalaryModel model = new SalaryModel();
     SalaryController controller = new SalaryController(model);
