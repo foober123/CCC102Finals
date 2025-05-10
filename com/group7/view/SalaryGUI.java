3b63f9500f78ffc950324cd6a986e455d01cb514
Charlson <fabrigar.charls@gmail.com>
2025-05-10 12:05:26 +0800

    Boilerplate to View

--- com/group7/view/SalaryGUI.java
+++ com/group7/view/SalaryGUI.java
@@ -1,3 +1,23 @@
 package com.group7.view;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
+public class SalaryGUI{
+private SalaryController controller;
+private JFrame frame;
+private JTextField hourlyWagefield;
+private JTextField hoursPerWeekfield;
+private JTextField taxRateField;
+
+}
+
+public void createAndShowGUI(){
+frame = new JFrame("Salary Calculator");
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setSize(350, 200);
+frame.setLocationRelativeTo(null);
+
+frame.setVisible(true);
+}
