95f530fadfb3855fd2de94bbc6d47599db974961
adnut143latik <andreplayz80@gmail.com>
2025-05-12 01:22:04 +0800

    Update SalaryController.java

--- src/com/group7/controller/SalaryController.java
+++ src/com/group7/controller/SalaryController.java
@@ -15,5 +15,21 @@ public void setView(SalaryGUI view){
 this.view = view;
 }
 
+public void SalaryCalc(String hourlyWageStr, String hoursStr, String taxStr){
+    double wage = Double.parseDouble(hourlyWageStr);  
+        double hours = Double.parseDouble(hoursStr);      
+        double tax = Double.parseDouble(taxStr);        
+        
+        // Update model
+        model.sethourlyWage(wage);
+        model.sethoursperweek(hours);
+        model.settaxRate(tax);
+
+        double net = model.calculateNetPay();
+        view.displayResult(String.format("Net Annual: $%,.2f", net));
+                    view.displayResult(String.format("Net Annual: $%,.2f", net));
 
 }
+
+}
+
